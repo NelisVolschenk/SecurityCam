abf133c9e15aedb6dd25f1dcd999b32831e735eb
Nelis Volschenk <nelisvolschenk@gmail.com>
2022-03-22 10:06:41 +0200

    basic install

--- install.sh
+++ install.sh
@@ -7,34 +7,40 @@ echo "Sudo User: $CURRENTUSERNAME"
 
 SUDO=
 if [ "$UID" != "0" ]; then
-	if [ -e /usr/bin/sudo -o -e /bin/sudo ]; then
-		SUDO=sudo
-	else
-		echo '*** This quick installer script requires root privileges.'
-		exit 0
-	fi
+  if [ -e /usr/bin/sudo -o -e /bin/sudo ]; then
+    SUDO=sudo
+  else
+    echo '*** This quick installer script requires root privileges.'
+    exit 0
+  fi
 fi
 
 $SUDO apt update
 $SUDO apt install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common \
-     git
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg2 \
+  software-properties-common \
+  git
 
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | $SUDO apt-key add -
+curl -fsSL https://download.docker.com/linux/$(
+  . /etc/os-release
+  echo "$ID"
+)/gpg | $SUDO apt-key add -
 
-echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-     $(lsb_release -cs) stable" | \
-    $SUDO tee /etc/apt/sources.list.d/docker.list
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(
+  . /etc/os-release
+  echo "$ID"
+) \
+     $(lsb_release -cs) stable" |
+  $SUDO tee /etc/apt/sources.list.d/docker.list
 
 $SUDO apt update
 
 $SUDO apt install -y --no-install-recommends \
-    docker-ce \
-    cgroupfs-mount
+  docker-ce \
+  cgroupfs-mount
 
 $SUDO systemctl enable docker
 $SUDO systemctl start docker
@@ -44,9 +50,9 @@ $SUDO apt install -y python3-pip libffi-dev
 $SUDO pip3 install docker-compose
 
 $SUDO usermod -a -G docker $CURRENTUSERNAME
-$SUDO exec sg docker newgrp `id -gn`
+$SUDO exec sg docker newgrp $(id -gn)
 
-cd /home/$CURRENTUSERNAME
+cd /home/"${CURRENTUSERNAME}"
 git clone https://github.com/ParagonIntegrations/SecurityCam.git
 
 cd /home/$CURRENTUSERNAME/SecurityCam/volumes/homeassistant
